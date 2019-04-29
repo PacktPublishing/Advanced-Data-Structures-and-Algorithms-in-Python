e0494f80b4f7afcd5ff168d9c9ecb60eb8d19c2d
ionescu vlad <ionescu.vlad1@gmail.com>
2019-04-29 15:27:51 +0300

    Fixed bug for 6.5 treaps and finished the code.

--- Section 6/5_treaps.py
+++ Section 6/5_treaps.py
@@ -10,6 +10,21 @@ class Node:
         self.right = None
 
 
+def get_height_and_check_priorities(node):
+    if node is None:
+        return 0
+
+    if node.left:
+        assert node.priority <= node.left.priority
+    if node.right:
+        assert node.priority <= node.right.priority
+
+    return 1 + max(
+        get_height_and_check_priorities(node.left),
+        get_height_and_check_priorities(node.right)
+    )
+
+
 def search(node, key):
     if not node:
         return False
@@ -54,6 +69,7 @@ def balance_tree(node):
     return node
 
 
+
 def insert(node, key):
     if not node:
         return Node(key, randint(1, 10000))
@@ -69,47 +85,62 @@ def remove(node, key):
     if not node:
         return node
     if node.key == key:
-        return None
+        if node.left is None and node.right is None:
+            return None
+        if node.left is None:
+            return node.right
+        if node.right is None:
+            return node.left
+        if node.left.priority < node.right.priority:
+            return remove(rotate_right(node), key)
+        return remove(rotate_left(node), key)
 
     if key < node.key:
         node.left = remove(node.left, key)
     else:
         node.right = remove(node.right, key)
-    return balance_tree(node)
+    return node
 
 
 python_set = set()
 treap = None
-for i in range(10000):
+for i in range(100000):
 
-    op = randint(1, 3)
+    if (i + 1) % 100 == 0:
+        print('Height after {} operations and {} values: {}'.format(
+            i,
+            len(python_set),
+            get_height_and_check_priorities(treap)
+        ))
+
+    op = randint(1, 10)
     if op == 1:
         val = randint(1, 1000)
-        print('Searching {}\npython set={}\nour treap={}\n'.format(
-            val,
-            sorted(python_set),
-            inorder(treap)))
+        # print('Searching {}\npython set={}\nour treap={}\n'.format(
+        #     val,
+        #     sorted(python_set),
+        #     inorder(treap)))
         assert (val in python_set) == search(treap, val),\
             'mismatch in search, set={}, treap={} value={}'.format(
                 val in python_set,
                 search(treap, val),
                 val
             )
-    elif op == 2:
-        val = randint(1, 1000)
-        print('Adding {}\npython set={}\nour treap={}\n'.format(
-            val,
-            sorted(python_set),
-            inorder(treap)))
+    elif op < 10:
+        val = randint(1, 2000)
+        # print('Adding {}\npython set={}\nour treap={}\n'.format(
+        #     val,
+        #     sorted(python_set),
+        #     inorder(treap)))
         python_set.add(val)
         if not search(treap, val):
             treap = insert(treap, val)
     else:
-        val = randint(1, 1000)
-        print('Removing {}\npython set={}\nour treap={}\n'.format(
-            val,
-            sorted(python_set),
-            inorder(treap)))
+        val = randint(1, 2000)
+        # print('Removing {}\npython set={}\nour treap={}\n'.format(
+        #     val,
+        #     sorted(python_set),
+        #     inorder(treap)))
         if val in python_set:
             python_set.remove(val)
         treap = remove(treap, val)
